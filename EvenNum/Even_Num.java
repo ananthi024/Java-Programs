c831cd2341e01ab180690c39d95c0fc3153c4a2d
ananthi024 <ananthiariya@gmail.com>
2017-05-29 15:21:19 -0800

    This is my first commit

---
+++ EvenNum/Even_Num.java
@@ -0,0 +1,19 @@
+class Even_Num
+{
+public int n=5;
+public static void main(String[] args)
+{
+Even_Num obj=new Even_Num();
+System.out.println("Enter Your Integer Number:"+obj.n);
+if(obj.n % 2 == 0)
+{
+System.out.println("The Number Is Even");
+}
+else
+{
+System.out.println("The Number Is Not Even");
+}
+}
+}
+
+
